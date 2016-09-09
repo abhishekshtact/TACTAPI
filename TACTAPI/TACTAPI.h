d3248ebc74ff4ad8f00960f3b0387b3a6fabcab3
abhishekshtact <dba@a7bhishek>
2016-09-09 10:16:12 +0530

    Initial Commit

---
+++ TACTAPI/TACTAPI.h
@@ -0,0 +1,19 @@
+//
+//  TACTAPI.h
+//  TACTAPI
+//
+//  Created by admin on 09/09/16.
+//  Copyright © 2016 TACT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TACTAPI.
+FOUNDATION_EXPORT double TACTAPIVersionNumber;
+
+//! Project version string for TACTAPI.
+FOUNDATION_EXPORT const unsigned char TACTAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TACTAPI/PublicHeader.h>
+
+
